8d009f42e3392c0e36f835893d5f2725e6d812a9
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2024-01-04 22:28:24 -0600

    MyApp

--- myapp/views.py
+++ myapp/views.py
@@ -1,55 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Project, Task
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import CreateNewTask, CreateNewProject
+
 # Create your views here.
-
-def index(request):
-    return render(request, 'index.html')
-
-def hello(request, username):
-    print(username)
-    return HttpResponse("<h1>Hello %s</h1>" %username)
+def hellow(request):
+    return HttpResponse("<h1>Hello World</h1>")
 
 def about(request):
-    return render(request, 'about.html')
-
-def projects(request):
-    projects = list(Project.objects.values())
-    return render (request, 'project/projects.html', {
-        'projects': projects
-    })
-
-
-def tasks(request):
-    tasks = Task.objects.all()
-    return render (request, 'task/task.html', {
-        'tasks': tasks
-    })
-
-def create_task(request):
-    if request.method == 'GET':
-        return render(request, "task/create_task.html", {
-            'form': CreateNewTask()
-        })
-    else:
-        Task.objects.create(tittle=request.POST['tittle'], description=request.POST['description'], project_id=2)
-        return redirect('task/tasks/')
-
-def create_project(request):
-    if request.method =='GET':
-        return render(request, 'project/create_project.html', {
-            'form': CreateNewProject()
-        })
-    else:
-        Project.objects.create(name=request.POST['name'])
-        return redirect('projects')
-    
-def project_detail(request, id):
-    project = get_object_or_404(Project, id = id)
-    tasks = Task.objects.filter(project_id = id)
-    return render(request, 'project/project_detail.html', {
-        'project': project,
-        'tasks': tasks
-    })
+    return HttpResponse("About")
